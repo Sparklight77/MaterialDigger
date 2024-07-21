66bffed22a5af2ef313783f1db3abedd425ff875
Sparklight77 <143299299+Sparklight77@users.noreply.github.com>
2024-07-21 20:42:08 +0500

    Update material.sh

--- material.sh
+++ material.sh
@@ -59,10 +59,10 @@ auto_detect() {
 
 # Prompt user to choose the architecture
 echo -e "${BOLD_BLUE}Please select the architecture:${RESET}"
-echo -e "${BOLD_BLUE}1) aarch64 ${RESET}"
-echo -e "${BOLD_BLUE}2) armv7 ${RESET}"
-echo -e "${BOLD_BLUE}3) Linux x86_64 ${RESET}"
-echo -e "${BOLD_BLUE}4) Auto-detect ${RESET}"
+echo -e "${BOLD_BLUE} 1) aarch64 ${RESET}"
+echo -e "${BOLD_BLUE} 2) armv7 ${RESET}"
+echo -e "${BOLD_BLUE} 3) Linux x86_64 ${RESET}"
+echo -e "${BOLD_BLUE} 4) Auto-detect ${RESET}"
 read -p "$(echo -e ${BOLD_RED}Enter the number corresponding to the desired architecture:${RESET} ) " arch
 
 # Set variables according to the user's selection
